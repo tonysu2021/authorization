151621cdaa7ac8e3fc06a85acda3805b3f7dfd85
Tony.su <tony.su@rhf-tech.com>
2022-01-26 18:09:05 +0800

    Add Mapper for permission

---
+++ auth-server/src/main/java/com/auth/server/app/mapper/PermissionMapper.java
@@ -0,0 +1,18 @@
+package com.auth.server.app.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import com.auth.server.app.dto.response.PermissionResponse;
+import com.auth.server.domain.entity.TbPermission;
+
+@Mapper(componentModel = "spring")
+public interface PermissionMapper {
+	
+	@Mapping(source = "parentId", target = "parentId")
+	@Mapping(source = "id", target = "id")
+	@Mapping(source = "name", target = "name")
+	@Mapping(source = "enname", target = "enname")
+	@Mapping(source = "url", target = "url")
+	public PermissionResponse entityToDto(TbPermission entity);
+}
--- auth-server/src/main/java/com/auth/server/app/service/PerService.java
+++ auth-server/src/main/java/com/auth/server/app/service/PerService.java
@@ -8,22 +8,22 @@ import java.util.Optional;
 import java.util.Queue;
 import java.util.Set;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.auth.server.app.dto.response.PermissionResponse;
+import com.auth.server.app.mapper.PermissionMapper;
 import com.auth.server.domain.entity.TbPermission;
 import com.auth.server.domain.service.TbPerService;
 
 @Service
 public class PerService {
 
-	private static final Logger logger = LoggerFactory.getLogger(PerService.class);
-
 	@Autowired
 	private TbPerService tbPerService;
+	
+	private PermissionMapper mapper = Mappers.getMapper(PermissionMapper.class);
 
 	/**
 	 * 使用廣度優先搜尋方式。
@@ -37,21 +37,19 @@ public class PerService {
 		if (root == null) {
 			return null;
 		}
-		logger.info("root : {}", root.getId());
-		PermissionResponse start = entityToDTO(root);
+		PermissionResponse start = mapper.entityToDto(root);
 		queue.offer(start);
 		visited.add(start.getId());
 		while (!queue.isEmpty()) {
 			int sz = queue.size();
 			for (int i = 0; i < sz; i++) {
 				PermissionResponse cur = queue.poll();
-				Optional<List<TbPermission>> neighborsOpt = tbPerService.findByParentId(cur.getId());
-				neighborsOpt.ifPresent((List<TbPermission> neighbors) -> {
+				Optional<List<TbPermission>> subPermissionsOpt = tbPerService.findByParentId(cur.getId());
+				subPermissionsOpt.ifPresent((List<TbPermission> neighbors) -> {
 					cur.setSubPermissions(new ArrayList<>());
 					List<PermissionResponse> subPermissions = cur.getSubPermissions();
 					for (TbPermission subPerEntity : neighbors) {
-						logger.info("subPer : {}", subPerEntity.getId());
-						PermissionResponse subPer = entityToDTO(subPerEntity);
+						PermissionResponse subPer = mapper.entityToDto(subPerEntity);
 						if (!visited.contains(subPer.getId())) {
 							queue.offer(subPer);
 							visited.add(subPer.getId());
@@ -62,15 +60,5 @@ public class PerService {
 			}
 		}
 		return start;
-	}
-
-	private PermissionResponse entityToDTO(TbPermission entity) {
-		PermissionResponse permission = new PermissionResponse();
-		permission.setParentId(entity.getParentId());
-		permission.setId(entity.getId());
-		permission.setName(entity.getName());
-		permission.setEnname(entity.getEnname());
-		permission.setUrl(entity.getUrl());
-		return permission;
 	}
 }
